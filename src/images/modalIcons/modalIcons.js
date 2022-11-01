a350dafe10b42f0819ec3a85f58ce86e9138265c
Emily <emilydta@users.noreply.github.com>
2022-11-01 15:35:57 +1100

    add icons/images

---
+++ src/images/modalIcons/modalIcons.js
@@ -0,0 +1,7 @@
+const modalIcons = {
+    cj: require('./cj.png'),
+    flick: require('./flick.png'),
+    bells: require('./bells.png')
+}
+
+export default modalIcons;
---
+++ src/images/modalIcons/shadowIcons/shadowIcons.js
@@ -0,0 +1,12 @@
+const shadowIcons = {
+    smallest1: require('./smallest_1.png'),
+    small2: require('./small_2.png'),
+    medium3: require('./medium_3.png'),
+    medium4: require('./medium_4.png'),
+    large5: require('./large_5.png'),
+    largest6: require('./largest_6.png'),
+    largestWithFin: require('./largest_with_fin_6.png'),
+    narrow: require('./narrow.png')
+}
+
+export default shadowIcons;
