3aa99c43cd8e1f74391be5c42e309eae56d21ae1
Emily <emilydta@users.noreply.github.com>
2022-11-08 13:23:09 +1100

    fix lingering icon spin after load

--- src/components/ProgressiveImage.js
+++ src/components/ProgressiveImage.js
@@ -1,6 +1,6 @@
 import './stylesheets/LoadingStyles.css';
 
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function ProgressiveImage({
     loadingIconSrc,
@@ -17,15 +17,20 @@ function ProgressiveImage({
             setImgSrc(src);
         };
     }, [src])
+
+    const isLoading = loadingIconSrc && imgSrc === loadingIconSrc;
     const loadingClass = loadingIconSrc && imgSrc === loadingIconSrc ? "loading" : "loaded";
+    const WrapComponent = isLoading ? 'div' : React.Fragment;
 
     return (
-        <img
-            {...{ src: imgSrc, ...props }}
-            alt={props.alt || ""}
-            className={`${imgClass} ${loadingClass}`}
-            style={{ filter: `${imgSrc.includes('images') ? 'drop-shadow(0.5em 0em 0.5em rgba(0, 0, 0, 0.48))' : 'none'}` }}
-        />
+        <WrapComponent>
+            <img
+                {...{ src: imgSrc, ...props }}
+                alt={props.alt || ""}
+                className={`${imgClass} ${loadingClass}`}
+                style={{ filter: `${imgSrc.includes('images') ? 'drop-shadow(0.5em 0em 0.5em rgba(0, 0, 0, 0.48))' : 'none'}` }}
+            />
+        </WrapComponent>
     )
 }
 
