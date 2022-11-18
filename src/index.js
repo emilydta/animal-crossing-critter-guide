b6665f9740c8b7691153ee3ebc9aa3ce91d8d2ed
Emily <emilydta@users.noreply.github.com>
2022-11-18 16:42:37 +1100

    change font loading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '@fontsource/nunito';
+import '@fontsource/nunito/900.css';
+import '@fontsource/nunito/800.css';
+import '@fontsource/nunito/700.css';
+import '@fontsource/nunito/600.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
