1bcb52557fb5b0aecb62527cbc5ee240e6434897
Emily <emilydta@users.noreply.github.com>
2022-11-25 18:57:22 +1100

    prepare for pwa

--- src/index.js
+++ src/index.js
@@ -6,9 +6,15 @@ import '@fontsource/nunito/900.css';
 import '@fontsource/nunito/800.css';
 import '@fontsource/nunito/700.css';
 import '@fontsource/nunito/600.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
